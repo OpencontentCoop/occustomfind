5c347aa084331a0025c6b387a2aa6627514297fa
Luca Realdi <luca.realdi@opencontent.it>
2024-09-11 16:06:49 +0200

    Allow html in dataset

--- design/standard/javascript/jquery.opendatadatasetview.js
+++ design/standard/javascript/jquery.opendatadatasetview.js
@@ -86,7 +86,7 @@
         }
 
         function autoLink(text) {
-            if (text) {
+            if (text && !text.startsWith("<ul>") && !text.startsWith("<p>")) {
                 if (isEmail(text)) {
                     return '<a href="mailto:' + text + '">' + text + '</a>';
                 }
