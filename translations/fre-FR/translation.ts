671246dc4f17f2760f6c848e097f5881f8902f31
Luca Realdi <luca.realdi@opencontent.it>
2025-06-03 11:37:41 +0200

    Add fre-FR translations

---
+++ translations/fre-FR/translation.ts
@@ -0,0 +1,487 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr">
+  <context>
+    <name>opendatadataset</name>
+    <message>
+      <source>Address</source>
+      <translation>Adresse</translation>
+    </message>
+    <message>
+      <source>API</source>
+      <translation>API</translation>
+    </message>
+    <message>
+      <source>API Doc</source>
+      <translation>Documentation API</translation>
+    </message>
+    <message>
+      <source>Are you sure you are removing all data from the dataset?</source>
+      <translation>Confirmez-vous la suppression de toutes les données de la ressource ?</translation>
+    </message>
+    <message>
+      <source>Are you sure you want to delete the record?</source>
+      <translation>Voulez-vous vraiment supprimer l’enregistrement ?</translation>
+    </message>
+    <message>
+      <source>Automatic import enabled</source>
+      <translation>Importation automatique activée</translation>
+    </message>
+    <message>
+      <source>Calendar</source>
+      <translation>Calendrier</translation>
+    </message>
+    <message>
+      <source>Calendar settings</source>
+      <translation>Paramètres du calendrier</translation>
+    </message>
+    <message>
+      <source>Cancel operation</source>
+      <translation>Annuler l’opération</translation>
+    </message>
+    <message>
+      <source>Chart</source>
+      <translation>Graphique</translation>
+    </message>
+    <message>
+      <source>Chart settings</source>
+      <translation>Paramètres du graphique</translation>
+    </message>
+    <message>
+      <source>Checkbox</source>
+      <translation>Case à cocher</translation>
+    </message>
+    <message>
+      <source>counter</source>
+      <translation>Compteur</translation>
+    </message>
+    <message>
+      <source>Counter</source>
+      <translation>Compteur</translation>
+    </message>
+    <message>
+      <source>Create new %name</source>
+      <translation>Ajouter %name</translation>
+    </message>
+    <message>
+      <source>CSV Dataset</source>
+      <translation>Ensemble de données au format CSV</translation>
+    </message>
+    <message>
+      <source>Daily</source>
+      <translation>Quotidienne</translation>
+    </message>
+    <message>
+      <source>Data table</source>
+      <translation>Tableau</translation>
+    </message>
+    <message>
+      <source>Date</source>
+      <translation>Date</translation>
+    </message>
+    <message>
+      <source>Date and time</source>
+      <translation>Date/heure</translation>
+    </message>
+    <message>
+      <source>Date format</source>
+      <translation>Format de date</translation>
+    </message>
+    <message>
+      <source>Date time format</source>
+      <translation>Format de date et heure</translation>
+    </message>
+    <message>
+      <source>Day</source>
+      <translation>Jour</translation>
+    </message>
+    <message>
+      <source>Default value</source>
+      <translation>Valeur par défaut</translation>
+    </message>
+    <message>
+      <source>Default view</source>
+      <translation>Aspect par défaut</translation>
+    </message>
+    <message>
+      <source>Delete</source>
+      <translation>Supprimer</translation>
+    </message>
+    <message>
+      <source>Delete data</source>
+      <translation>Supprimer les données</translation>
+    </message>
+    <message>
+      <source>Disable</source>
+      <translation>Désactiver</translation>
+    </message>
+    <message>
+      <source>Download CSV</source>
+      <translation>Télécharger au format CSV</translation>
+    </message>
+    <message>
+      <source>Edit fields definition</source>
+      <translation>Modifier les définitions des champs</translation>
+    </message>
+    <message>
+      <source>Edit views settings</source>
+      <translation>Modifier les paramètres d’affichage</translation>
+    </message>
+    <message>
+      <source>Email</source>
+      <translation>E-mail</translation>
+    </message>
+    <message>
+      <source>Enable automatic update</source>
+      <translation>Activer les mises à jour automatiques</translation>
+    </message>
+    <message>
+      <source>Enable create/edit/remove single item?</source>
+      <translation>Activer la création/modification/suppression d’un seul élément ?</translation>
+    </message>
+    <message>
+      <source>End date field</source>
+      <translation>Champ de date de fin de l’événement</translation>
+    </message>
+    <message>
+      <source>Enum</source>
+      <translation>Liste</translation>
+    </message>
+    <message>
+      <source>Fields</source>
+      <translation>Champs</translation>
+    </message>
+    <message>
+      <source>Filter by</source>
+      <translation>Filtrer par</translation>
+    </message>
+    <message>
+      <source>Frequency</source>
+      <translation>Fréquence</translation>
+    </message>
+    <message>
+      <source>Geo location (longitude and latitude)</source>
+      <translation>Localisation géographique (longitude et latitude)</translation>
+    </message>
+    <message>
+      <source>Geo location separator</source>
+      <translation>Séparateur de chaîne de localisation géographique</translation>
+    </message>
+    <message>
+      <source>I understand the consequences, delete this dataset</source>
+      <translation>D’accord, j’ai compris, supprimer tout</translation>
+    </message>
+    <message>
+      <source>Identifier</source>
+      <translation>Identifiant</translation>
+    </message>
+    <message>
+      <source>Import</source>
+      <translation>Importer</translation>
+    </message>
+    <message>
+      <source>Import data from CSV</source>
+      <translation>Importer depuis un fichier CSV</translation>
+    </message>
+    <message>
+      <source>Import definitions from CSV</source>
+      <translation>Importer les définitions des champs depuis un fichier CSV</translation>
+    </message>
+    <message>
+      <source>Import fields definition from csv</source>
+      <translation>Importer les définitions des champs depuis un fichier CSV</translation>
+    </message>
+    <message>
+      <source>Import fields definition from Google Spreadsheet</source>
+      <translation>Importer les définitions des champs depuis Google Sheets</translation>
+    </message>
+    <message>
+      <source>Import from CSV</source>
+      <translation>Importer des données depuis un fichier CSV</translation>
+    </message>
+    <message>
+      <source>Import from Google Sheet</source>
+      <translation>Importer depuis Google Sheets</translation>
+    </message>
+    <message>
+      <source>Include weekends?</source>
+      <translation>Afficher le week-end ?</translation>
+    </message>
+    <message>
+      <source>Integer</source>
+      <translation>En entier</translation>
+    </message>
+    <message>
+      <source>Invalid spreadsheet url</source>
+      <translation>URL Google Sheets non valide</translation>
+    </message>
+    <message>
+      <source>Is required?</source>
+      <translation>Obligatoire ?</translation>
+    </message>
+    <message>
+      <source>Item name</source>
+      <translation>Nom de l’élément</translation>
+    </message>
+    <message>
+      <source>Label</source>
+      <translation>Étiquette</translation>
+    </message>
+    <message>
+      <source>Latitude</source>
+      <translation>Latitude</translation>
+    </message>
+    <message>
+      <source>Longitude</source>
+      <translation>Longitude</translation>
+    </message>
+    <message>
+      <source>map</source>
+      <translation>Plan</translation>
+    </message>
+    <message>
+      <source>Map</source>
+      <translation>Plan</translation>
+    </message>
+    <message>
+      <source>MomentJS date format (es: DD/MM/YYYY)</source>
+      <translation>Format de date selon la norme MomentJS (exemple : JJ/MM/AAAA)</translation>
+    </message>
+    <message>
+      <source>MomentJS datetime format (es: DD/MM/YYYY HH:mm)</source>
+      <translation>Format de date et heure selon la norme MomentJS (exemple : JJ/MM/AAAA HH:mm)</translation>
+    </message>
+    <message>
+      <source>Month</source>
+      <translation>Mois</translation>
+    </message>
+    <message>
+      <source>Monthly</source>
+      <translation>Mensuelle</translation>
+    </message>
+    <message>
+      <source>No results finding</source>
+      <translation>Aucun résultat</translation>
+    </message>
+    <message>
+      <source>Number</source>
+      <translation>Numéro</translation>
+    </message>
+    <message>
+      <source>One item per line</source>
+      <translation>Un élément par ligne</translation>
+    </message>
+    <message>
+      <source>Please type %name to confirm.</source>
+      <translation>Veuillez taper %name pour confirmer</translation>
+    </message>
+    <message>
+      <source>Removing your existing data from the dataset</source>
+      <translation>Supprimer toutes vos données existantes</translation>
+    </message>
+    <message>
+      <source>Schedule</source>
+      <translation>Programmer</translation>
+    </message>
+    <message>
+      <source>Select</source>
+      <translation>Sélectionner</translation>
+    </message>
+    <message>
+      <source>Select Google Spreadsheet Uri</source>
+      <translation>Sélectionner l’URL de Google Sheets</translation>
+    </message>
+    <message>
+      <source>Selects additional users who can edit elements of this dataset in addition to editors</source>
+      <translation>Sélectionner les utilisateurs qui peuvent modifier les éléments de cette ressource (en plus des rédacteurs)</translation>
+    </message>
+    <message>
+      <source>Set fields definition</source>
+      <translation>Configurer les définitions des champs</translation>
+    </message>
+    <message>
+      <source>Set views settings</source>
+      <translation>Créer les paramètres d’affichage</translation>
+    </message>
+    <message>
+      <source>Show fields</source>
+      <translation>Afficher les champs</translation>
+    </message>
+    <message>
+      <source>Show filters</source>
+      <translation>Afficher les filtres</translation>
+    </message>
+    <message>
+      <source>Show label</source>
+      <translation>Afficher l’étiquette</translation>
+    </message>
+    <message>
+      <source>Start date and time</source>
+      <translation>Date et heure initiales</translation>
+    </message>
+    <message>
+      <source>Start date field</source>
+      <translation>Champ de date de début de l’événement</translation>
+    </message>
+    <message>
+      <source>String</source>
+      <translation>Chaîne</translation>
+    </message>
+    <message>
+      <source>Table settings</source>
+      <translation>Paramètres du tableau</translation>
+    </message>
+    <message>
+      <source>Text event fields</source>
+      <translation>Champs affichés dans le texte de l’événement</translation>
+    </message>
+    <message>
+      <source>Textarea</source>
+      <translation>Texte</translation>
+    </message>
+    <message>
+      <source>The expected value is a string consisting of longitude + separator + latitude</source>
+      <translation>La valeur attendue est une chaîne composée de longitude, séparateur, latitude</translation>
+    </message>
+    <message>
+      <source>There are data being updated</source>
+      <translation>Données en cours de mise à jour...</translation>
+    </message>
+    <message>
+      <source>try to refine your search keywords</source>
+      <translation>Essayer d’affiner les termes de recherche</translation>
+    </message>
+    <message>
+      <source>Type</source>
+      <translation>Type</translation>
+    </message>
+    <message>
+      <source>URL</source>
+      <translation>URL</translation>
+    </message>
+    <message>
+      <source>Users who can enter or modify data</source>
+      <translation>Utilisateurs pouvant saisir ou modifier des données</translation>
+    </message>
+    <message>
+      <source>Views</source>
+      <translation>Affichages</translation>
+    </message>
+    <message>
+      <source>Week</source>
+      <translation>Semaine</translation>
+    </message>
+    <message>
+      <source>Weekly</source>
+      <translation>Hebdomadaire</translation>
+    </message>
+    <message>
+      <source>Remove all data and settings</source>
+      <translation>Supprimer toutes les données et les configurations</translation>
+    </message>
+    <message>
+      <source>Cancel</source>
+      <translation>Annuler</translation>
+    </message>
+    <message>
+      <source>Go to source</source>
+      <translation>Aller à la source de données</translation>
+    </message>
+    <message>
+      <source>The latitude and longitude format: you can use %latitude and %longitude placeholders, the %latitude and %longitude expecetd values must be float values (with dot as decimal separator, e.g. 43.1234)</source>
+      <translation>Précisez le format de latitude et de longitude : vous pouvez utiliser les espaces réservés %latitude et %longitude, les valeurs attendues dans les données sont de type décimal (avec le point comme séparateur décimal, par exemple, 43.1234)</translation>
+    </message>
+    <message>
+      <source>Are you sure you are removing all data and settings?</source>
+      <translation>Confirmez-vous la suppression de toutes les données et les configurations de la ressource ?</translation>
+    </message>
+    <message>
+      <source>Counter settings</source>
+      <translation>Paramètres du compteur</translation>
+    </message>
+    <message>
+      <source>Minimun</source>
+      <translation>Minimum</translation>
+    </message>
+    <message>
+      <source>Maximun</source>
+      <translation>Maximum</translation>
+    </message>
+    <message>
+      <source>Count</source>
+      <translation>Nombre total d’éléments</translation>
+    </message>
+    <message>
+      <source>Missing values</source>
+      <translation>Éléments non remplis</translation>
+    </message>
+    <message>
+      <source>Sum</source>
+      <translation>Somme</translation>
+    </message>
+    <message>
+      <source>Mean</source>
+      <translation>Moyenne</translation>
+    </message>
+    <message>
+      <source>Perform calculation on a single field</source>
+      <translation>Effectuer le calcul sur un seul champ</translation>
+    </message>
+    <message>
+      <source>Event limit</source>
+      <translation>Nombre d’événements affichés</translation>
+    </message>
+    <message>
+      <source>Counter label</source>
+      <translation>Étiquette</translation>
+    </message>
+    <message>
+      <source>Counter image full url</source>
+      <translation>Image (saisir l’URL complète)</translation>
+    </message>
+    <message>
+      <source>Field</source>
+      <translation>Champ</translation>
+    </message>
+    <message>
+      <source>Select field</source>
+      <translation>Sélectionner un champ</translation>
+    </message>
+    <message>
+      <source>Select calculation</source>
+      <translation>Sélectionner le calcul</translation>
+    </message>
+    <message>
+      <source>Show search input</source>
+      <translation>Afficher le moteur de recherche</translation>
+    </message>
+    <message>
+      <source>Show search input in text fields</source>
+      <translation>Afficher l’entrée pour effectuer une recherche dans les champs de type texte</translation>
+    </message>
+    <message>
+      <source>URL with label (html a element with href)</source>
+      <translation>URL avec étiquette (élément HTML a avec href)</translation>
+    </message>
+    <message>
+      <source>Opening view</source>
+      <translation>Affichage à l’ouverture</translation>
+    </message>
+    <message>
+      <source>View</source>
+      <translation>Affichage</translation>
+    </message>
+    <message>
+      <source>Default sorting method</source>
+      <translation>Tri par défaut</translation>
+    </message>
+    <message>
+      <source>Search and filters</source>
+      <translation>Filtres de recherche</translation>
+    </message>
+    <message>
+      <source>Search filters</source>
+      <translation>Filtres de recherche</translation>
+    </message>
+  </context>
+</TS>
